61a69d39321eba806b478d3e3e9ca5877a6fd467
JManciet <manciet@hotmail.fr>
2023-03-15 09:47:04 +0100

    Création entité Favori

---
+++ src/Entity/Favori.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FavoriRepository::class)
+ */
+class Favori
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
