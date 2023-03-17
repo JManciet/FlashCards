1c4ccae62ec5024fd4f2a7d344b9219d4e36c5da
JManciet <manciet@hotmail.fr>
2023-03-17 09:37:15 +0100

    Récupération utilisateurs dans BDD

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UtilisateurController extends AbstractController
 {
     /**
      * @Route("/utilisateur", name="app_utilisateur")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+
+        $utilisateurs = $doctrine->getRepository(Utilisateur::class)->findAll();
+
         return $this->render('utilisateur/index.html.twig', [
             'controller_name' => 'UtilisateurController',
+            'utilisateurs' => $utilisateurs
         ]);
     }
 }
