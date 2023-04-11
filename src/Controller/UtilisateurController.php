732588d27202c456c9e0f5175de9b95f4307d4af
JManciet <manciet@hotmail.fr>
2023-04-11 21:45:56 +0200

    changement de pseudo opérationelle

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UtilisateurController extends AbstractController
 {
@@ -30,6 +34,53 @@ class UtilisateurController extends AbstractController
     {
         return $this->render('utilisateur/show.html.twig', [
             'utilisateur' => $utilisateur
+        ]);
+    }
+
+    /**
+     * @Route("/mon_profil", name="my_profile_user")
+     */
+    public function profil(Request $request , EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+
+        $user = $this->getUser();
+
+        if(!$user) return $this->redirectToRoute('app_login');
+
+
+        $form = $this->createForm(UtilisateurType::class, $user);
+
+        $form->handleRequest($request);
+        // dd($user);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // dd($form->get('password')->getData());
+            if ($hasher->isPasswordValid($user, $form->get('password')->getData())) {
+                // dd($user);
+                // dd('aaa');
+                // Mettre à jour le pseudo de l'utilisateur
+                $user->setPseudo($form->get('pseudo')->getData());
+    
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Le pseudo à bien été modifié.'
+                );
+
+                return $this->redirectToRoute('my_profile_user');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
+            }
+        }
+
+
+        return $this->render('utilisateur/my_profile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UtilisateurType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('pseudo', TextType::class, [
+            'mapped' => false,
+            'attr' => [
+                'class' => 'form-control',
+                'minlenght' => '2',
+                'maxlenght' => '50',
+            ],
+            'required' => true,
+            'label' => 'Pseudo',
+            'label_attr' => [
+                'class' => 'form-label  mt-4'
+            ],
+            'constraints' => [
+                // new Assert\Length(['min' => 2, 'max' => 50])
+            ]
+        ])
+        ->add('password', PasswordType::class, [
+            'mapped' => false,
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'label' => 'Mot de passe',
+            'label_attr' => [
+                'class' => 'form-label  mt-4'
+            ]
+        ])
+        ->add('submit', SubmitType::class, [
+            'attr' => [
+                'class' => 'btn btn-primary mt-4'
+            ]
+        ])
+        
+        ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
